5922458a2dd0dce197e21a72563f2b6d91fe370f
Cesar Puentes <caps2695@gmail.com>
2021-10-24 12:22:34 -0500

    separando a rutas por archivos

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const routes = require("./routes");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = 3070;
+
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
