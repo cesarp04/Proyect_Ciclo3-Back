58e2dfdfdf8ff36ecdbc13adf5aa08e5581db251
Cesar Puentes <caps2695@gmail.com>
2021-10-27 23:32:13 -0500

    creada la ruta para editar una venta

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const {
-  getVentasContorller,
-  createVentasController,
-} = require("../controllers/ventas.controller");
+const { getVentasContorller, createVentasController } = require("../controllers/ventas.controller");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -12,5 +9,6 @@ router.get("/", (req, res) => {
 // routes ventas
 router.get("/ventas", getVentasContorller);
 router.post("/ventas", createVentasController);
+router.put("/ventas", createVentasController);
 
 module.exports = router;
